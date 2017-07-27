51a245a59935a49b37d2fbed4694acce6190636f
kelly-keating <keating.kel@gmail.com>
2017-07-27 22:49:03 +1200

    prompts altered

--- anag.rb
+++ anag.rb
@@ -1,27 +1,30 @@
 start_time = Time.now
 
-6.times do 
+puts "Try and guess the scrambled word" 
+puts "(you can type ':hint' if you need a hand)"
 
+
+6.times do 
 	words = File.read("words.txt").split("\n").select{ |w| w.length > 4 && w.length < 7 }
 	word = words.sample
 
 	anag = word.split("").shuffle.join
 	not_guessed = true
 
-	puts word
-	puts anag
-	puts "What is this word??"
+	puts "--> #{anag}"
 
 	while not_guessed
 		guess = gets.chomp
 		if guess == word
 			puts "  You got it!"
 			not_guessed = false
-			break
+		elsif guess == ":hint"
+			puts "(try '#{word}')"
 		end
 	end
 end
 
 end_time = Time.now
 
+puts "Well done!!"
 puts "You took #{(end_time - start_time).floor} seconds"
--- numberGuesser.rb
+++ numberGuesser.rb
@@ -1,24 +1,23 @@
-
 actual = rand(1..100)
 won = false
 
 puts "Try and guess the number I am thinking of.."
-6.times do
-	guess = gets.chomp.to_i
-	if guess == actual
-		puts "you won!"
-		won = true
-		break
-	elsif guess < actual
-		puts "too low!"
-	else
-		puts "too high!"
+	6.times do
+		guess = gets.chomp.to_i
+		if guess == actual
+			puts "you won!"
+			won = true
+			break
+		elsif guess < actual
+			puts "too low!"
+		else
+			puts "too high!"
+		end
 	end
-end
 
-if won
-	puts "YOU WON!"
-else
-	puts "you lost :("
+	if won
+		puts "YOU WON!"	
+	else
+		puts "you lost :("
 
 end
