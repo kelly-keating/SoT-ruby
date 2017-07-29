d325b51849ccf797dd241a024af292630d266a21
kelly-keating <keating.kel@gmail.com>
2017-07-29 13:39:50 +1200

    challenge completed

--- HacFest/wordMatch.rb
+++ HacFest/wordMatch.rb
@@ -1,17 +1,33 @@
-def test(a1 = "Ruby", a2 = "Perl")
-   puts "The programming language is #{a1}"
-   puts "The programming language is #{a2}"
-end
-test "C", "C++"
-test
 
-givenDict = File.read("commonWords.txt").split("\n").select{ |w| w.length > 3 }
+def matcher(word = "SPLIT", dict = File.read("dictionary.txt").split("\n").select{ |w| w.length > 3 })
+  result = []
+  fours = []
+  word.upcase!
+  for x in dict
+    x.upcase!
+  end
 
-def matcher(word = "SPLIT", dict = givenDict)
-  # word.toUpper
-  # dict.every.toUpper
-  # word split to fours
-  # for each four search and return array
-  # join all arrays
-  # return final array
+  # store fours in array
+  $i = 0
+  while $i < word.length - 3  do
+    fours.push(word[$i..$i+3])
+    $i +=1
+  end
+
+  # pop from array into result
+  for letters in fours
+    for term in dict
+      if term.include? letters
+        result.push(term)
+        dict.delete(term)
+      end
+    end
+  end
+
+  # return resulting array
+  result.sort!
+  puts result
+  return result
 end
+
+matcher
